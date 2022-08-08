8914352f3b0443b73d085a073dcbbc30e9e2c271
Ramadito Ferdian Assa <ramaditoferdian@gmail.com>
2022-08-08 10:28:09 +0700

    rename service

--- campaign/service.go
+++ campaign/service.go
@@ -1,7 +1,7 @@
 package campaign
 
 type Service interface {
-	FindCampaigns(userID int) ([]Campaign, error)
+	GetCampaigns(userID int) ([]Campaign, error)
 }
 
 type service struct {
@@ -12,9 +12,9 @@ func NewService(repository Repository) *service {
 	return &service{repository}
 }
 
-func (s *service) FindCampaigns(userID int) ([]Campaign, error) {
+func (s *service) GetCampaigns(userID int) ([]Campaign, error) {
 	// todo : Saat userID ditemukan
-	if userID != 0 {
+	if userID > 0 {
 		campaigns, err := s.repository.FindByUserID(userID)
 		if err != nil {
 			return campaigns, err
